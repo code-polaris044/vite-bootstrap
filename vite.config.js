4bb7fdcb7cfdff0cbc76d4cbb090dc1e9736b36b
shun916 <118881278+code-polaris044@users.noreply.github.com>
2025-07-08 17:47:39 +0900

    static copy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,34 @@
 import { resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default {
 	root: resolve(__dirname, "resources"),
 	build: {
 		outDir: "../public",
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/js/[name].js",
+				chunkFileNames: "assets/js/[name].js",
+				assetFileNames: (assetInfo) => {
+					if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+						return "assets/css/[name][extname]";
+					}
+					return "assets/[name][extname]";
+				},
+			},
+		},
 	},
+	plugins: [
+		viteStaticCopy({
+			targets: [
+				{ src: "fonts/*", dest: "assets/fonts" },
+				{ src: "img/*", dest: "assets/img" },
+			],
+		}),
+	],
 	server: {
 		port: 8080,
 	},
-	// Optional: Silence Sass deprecation warnings. See note below.
 	css: {
 		preprocessorOptions: {
 			scss: {
