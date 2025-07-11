b433e998696e967d136de3caa4c32afdf958b88a
shun916 <118881278+code-polaris044@users.noreply.github.com>
2025-07-11 11:51:35 +0900

    flatpickr オプション追加

--- resources/js/components/flatpickr.js
+++ resources/js/components/flatpickr.js
@@ -5,26 +5,26 @@
 
 // public / assets / css / script.cssへ出力
 import "flatpickr/dist/flatpickr.min.css";
+//テーマ
+import "flatpickr/dist/themes/material_blue.css";
+//flatpickr
 import flatpickr from "flatpickr";
+// 期間選択のプラグイン
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 //日本語化対応
 import { Japanese } from "flatpickr/dist/l10n/ja.js";
 
 flatpickr("#datepicker", {
-	// minDate: "2023-01-01",
-	// maxDate: "2023-12-31",
-	enableTime: true,
-	dateFormat: "Y-m-d H:i",
-	disableMobile: "true", //モバイル対応
 	locale: Japanese,
+	dateFormat: "Y-m-d",
+	minDate: "today",
+	disableMobile: "true", //モバイル対応
 });
 
-flatpickr(".input-group-addon", {
-	// minDate: "2023-01-01",
-	// maxDate: "2023-12-31",
-	enableTime: true,
-	dateFormat: "Y-m-d H:i",
-	disableMobile: "true", //モバイル対応
+flatpickr("#firstRangeInput", {
+	plugins: [new rangePlugin({ input: "#secondRangeInput" })],
 	locale: Japanese,
+	dateFormat: "Y-m-d",
+	minDate: "today",
+	disableMobile: "true", //モバイル対応
 });
-
-
